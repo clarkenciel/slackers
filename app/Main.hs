c085d2565514b3e9504e34f814f9ef2c366288aa
clarkenciel <harklarke@gmail.com>
2016-07-22 15:03:11 -0400

    adding slack integrations

--- app/Main.hs
+++ app/Main.hs
@@ -7,15 +7,24 @@ import Web.Spock.Safe
 import Data.Maybe (fromMaybe)
 import Control.Monad.Trans (liftIO)
 import System.Environment (lookupEnv)
+
 main :: IO ()
 main = do
-  port <- portNumber
-  runSpock port $ spockT id $
-    get "vimgif" $ liftIO serveRandomVimGif >>= json
+  port <- getPortNumber
+  safeToken <- getSafeToken
+  runSpock port $ spockT id $ do
+    get "slackers" $ text "hello!"
+    get "slackers/vimgif" <//> var $ (\token -> 
+        if token == safeToken
+          then liftIO serveRandomVimGif >>= json
+          else text "nope!"
 
-portNumber = do
+getPortNumber = do
   -- once again, a double fmap since lookupEnv :: IO (Maybe String)
   -- so, <$> gets (fmap read) into IO and fmap gets read into Maybe.
   -- so, this gives us an IO (Maybe Int) (technically Read a => IO (Maybe a))
   port <- fmap read <$> lookupEnv "SLACKERS_PORT"
   return $ fromMaybe 8080 port
+
+getSafeToken = do
+  
