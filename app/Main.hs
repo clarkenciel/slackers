c5c4588efe7b3366efbe6979dffd7945036e4c2e
clarkenciel <danny@clarkenciel.com>
2016-09-05 22:16:16 -0700

    need to redo toJSON for slack messages, but on right path now

--- app/Main.hs
+++ app/Main.hs
@@ -8,31 +8,22 @@ import Web.Spock.Safe
 import Data.Maybe (fromMaybe)
 import Control.Monad.Trans (liftIO)
 import System.Environment (lookupEnv)
-import Data.Text (splitOn, lines)
+import Data.Text (Text(..), splitOn, lines)
 import Data.Text.IO (readFile)
-import Data.Map as M (lookup, fromList, keysSet)
+import Data.Map as M (Map(..), lookup, fromList, keysSet)
 import Data.Set as S ((\\), fromList, toList)
 
 main :: IO ()
 main = do
   port <- getPortNumber
   safeTokens <- getSafeTokens
+  print safeTokens
   let missingTokens = appList \\ keysSet safeTokens
    in if not (null missingTokens)
          then missingTokensError missingTokens
-         else
-           runSpock port $ spockT id $ do
-           get "slackers" $ text "hello!"
-           get ("slackers/vimgif" <//> var) (\token -> 
-             if token == lookup "vimgif" safeTokens
-                then liftIO serveRandomVimGif >>= json
-                else text "nope!")
-                  where missingTokensError = putStrLn . ("You are missing the following tokens from slackers.cfg, please provide them: " ++) . show . toList
+         else runSpock port $ spockT id (slackersAPI safeTokens)
 
 getPortNumber = do
-  -- once again, a double fmap since lookupEnv :: IO (Maybe String)
-  -- so, <$> gets (fmap read) into IO and fmap gets read into Maybe.
-  -- so, this gives us an IO (Maybe Int) (technically Read a => IO (Maybe a))
   port <- fmap read <$> lookupEnv "SLACKERS_PORT"
   return $ fromMaybe 8080 port
 
@@ -40,3 +31,16 @@ appList = S.fromList ["vimgif"]
 
 getSafeTokens = M.fromList . map eqToPair . lines <$> readFile "slackers.cfg"
     where eqToPair = (\(x:y:_) -> (x,y)) . splitOn "="
+
+slackersAPI tokenCollection = do
+  get "slackers" $ text "hello!"
+  get "slackers/vimgif" (liftIO serveRandomVimGif >>= json)
+
+withTokens tokenCollection tokenKey action token =
+  if token == lookup tokenKey tokenCollection
+     then action
+     else text "nope!"
+
+missingTokensError = 
+  putStrLn . ("You are missing the following tokens from slackers.cfg, please provide them: " ++) . show . toList
+
--- src/VimGif.hs
+++ src/VimGif.hs
@@ -1,112 +1,55 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-{- VimGif will provide a random gif from vimgif.com
-   This works fine, but could really benefit from using MaybeT.
-   Oh well, someday!
--}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, RecordWildCards #-}
 
 module VimGif (serveRandomVimGif) where
 
+import SlackTypes
 
-import qualified Text.XML as X
-import qualified Text.HTML.DOM as D
 import qualified Data.ByteString.Lazy as L
 import qualified Network.HTTP.Simple as S
-import qualified Control.Monad.Random as R
-import qualified Data.Aeson as A
-import Data.Maybe (fromJust, isJust)
-import Data.Text (Text, splitOn, append, unpack)
-import Data.String.Conversions (cs)
-import Prelude hiding (lookup)
-import Data.Map.Strict (lookup)
+import Data.Text (Text, pack)
+import Data.Maybe (fromMaybe)
+import Control.Monad.Random (MonadRandom, getRandomR)
+import Data.Aeson 
+  ( FromJSON, ToJSON, Value(Number, String)
+  , decode, encode, parseJSON)
+import GHC.Generics (Generic)
+import Data.Vector (Vector(..), (!), fromList)
 
-{- "MAIN" function -}
+-- | wrapping Text in a newtype
+-- this will get us the encoding/decoding JSON features
+-- and the wrapper disappears at runtime
+newtype GifTitle = GifTitle { t :: Text }
+  deriving (Show, Generic)
 
---serveRandomVimGif :: IO 
-serveRandomVimGif = do
-  pageLinks <- getVimGifPageLinks
-  case pageLinks of
-    Nothing -> return ["No gifs found"]
-    Just pls -> do
-      choice <- fst . R.randomR (0, length pls - 1) <$> R.newStdGen
-      gifPage <- S.parseRequest $ unpack (pls !! choice)
-      gifs <- getGifsFrom gifPage
-      case gifs of
-        Nothing -> return ["No gifs found"]
-        Just gs -> return gs
+instance ToJSON GifTitle
+instance FromJSON GifTitle where
+  parseJSON (String t) = pure $ GifTitle t
+  parseJSON (Number n) = pure (GifTitle $ pack (show n))
+  parseJSON _ = mempty
 
+data Gif
+  = Gif
+  { title :: GifTitle
+  , url :: Text
+  , awslink :: Text
+  } deriving (Show, Generic)
 
-{- RESPONSE PARSING -}
-safeHead l 
-  | null l = Nothing 
-  | otherwise = Just $ head l
+instance FromJSON Gif
+instance ToJSON Gif
 
-matchesName t = (== t) . X.nameLocalName . X.elementName
+type GifList = Vector Gif
 
-getElements :: [X.Node] -> [X.Element]
-getElements = map extract . filter isNodeElement
-  where extract (X.NodeElement e) = e
-        isNodeElement (X.NodeElement e) = True
-        isNodeElement _ = False
+serveRandomVimGif :: IO SlackMessage
+serveRandomVimGif = gifToSlack <$> (getGifs >>= chooseRandom)
 
-getChildren :: X.Element -> [X.Element]
-getChildren = getElements . X.elementNodes
+getGifs :: IO GifList
+getGifs = fromMaybe (fromList [Gif (GifTitle "") "" ""]) . decode . S.getResponseBody 
+          <$> S.httpLbs "https://vimgifs.com/gifs.json"
 
-getBody :: X.Document -> Maybe X.Element
-getBody = safeHead . bodyElements . getChildren . X.documentRoot
-  where bodyElements = filter isBody
-        isBody = (== "body") . X.nameLocalName . X.elementName
+chooseRandom :: (MonadRandom m) => Vector a -> m a
+chooseRandom gifs = do
+  idx <- getRandomR (0, length gifs - 1)
+  return $ gifs ! idx
 
-getTagsByName :: Text -> X.Element -> [X.Element]
-getTagsByName tagName = concatMap branch . getChildren
-  where branch element = if tagName `matchesName` element
-                            then [element]
-                            else getTagsByName tagName element
-
-
-getTagsIn tagName lookupFunc = 
-  extractValues . onlySuccess . map lookupFunc . getTagsByName tagName
-    where onlySuccess = filter isJust
-          extractValues = map fromJust
-
-getAttribute attrName = lookup attrName . X.elementAttributes
-
-getHrefsIn :: X.Element -> [Text]
-getHrefsIn = getTagsIn "a" (getAttribute "href")
-
-getImgsIn :: X.Element -> [Text]
-getImgsIn = getTagsIn "img" (getAttribute "src")
-
-lbsTo :: (X.Element -> a) -> L.ByteString -> Maybe a
-lbsTo f = fmap f . getBody . D.parseLBS
-
-lbsToHrefs :: L.ByteString -> Maybe [Text]
-lbsToHrefs = lbsTo getHrefsIn
-
-lbsToImg :: L.ByteString -> Maybe [Text]
-lbsToImg = lbsTo getImgsIn
-
-getGifLinks :: [Text] -> [Text]
-getGifLinks = filter (\h -> includesGif h && isHtml h)
-  where includesGif = ("_gifs" `elem`) . splitOn "/"
-        isHtml = (== "html") . htmlPart
-        htmlPart = last . splitOn "."
-
-getGifImgs :: [Text] -> [Text]
-getGifImgs = filter isGif
-  where isGif = ("gif" ==) . last . splitOn "."
-
-getGifPageLinks :: [Text] -> [Text]
-getGifPageLinks = map buildURL . getGifLinks
-  where buildURL stem =  "http://vimgifs.com/" `append` filename stem `append` "/"
-        filename = last . splitOn "/" . head . splitOn "."
-
-processBodyWith :: (L.ByteString -> a) -> S.Request -> IO a
-processBodyWith processor url = processor . S.getResponseBody <$> S.httpLbs url
-
--- note the two fmaps here to deal with the Maybe-inside-IO thing
-getVimGifPageLinks :: IO (Maybe [Text])
-getVimGifPageLinks =  processBodyWith (fmap getGifPageLinks . lbsToHrefs) "https://github.com/mrmrs/vimgifs/tree/gh-pages/_gifs"
-
-getGifsFrom :: S.Request -> IO (Maybe [Text])
-getGifsFrom = processBodyWith (fmap getGifImgs . lbsToImg)
+gifToSlack :: Gif -> SlackMessage
+gifToSlack Gif{..} = basicMessage (t title) +: imgAttachment awslink
